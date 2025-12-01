022cde33ce7381f8d078e6a5207c37f9b8385452
vinithreddybanda <vinithreddy08.123@gmail.com>
2025-12-02 00:16:55 +0530

    Add insert at end method

--- LinkedListDemo.java
+++ LinkedListDemo.java
@@ -23,14 +23,29 @@ class LinkedList {
         }
         System.out.println();
     }
+
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
 }
+
 
 public class LinkedListDemo {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
         list.insertAtBeginning(10);
+        list.insertAtEnd(40);
         list.insertAtBeginning(20);
-        list.insertAtBeginning(30);
-        list.printList(); // Output: 30 20 10
+        list.insertAtEnd(50);
+        list.printList(); // Output: 20 10 40 50
     }
 }
